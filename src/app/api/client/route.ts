9a256a34c79e80017331c780dd485f9aa86315a2
panudet ingai <codedroy3@gmail.com>
2024-09-01 23:14:58 +0700

    Refactor API route to use "opp003" table instead of "BO013" table

--- src/app/api/client/route.ts
+++ src/app/api/client/route.ts
@@ -12,7 +12,7 @@ export async function POST(req: Request) {
     try {
         const { ult_value, led} = await req.json();
         // Hash password
-        const res = await client.query('INSERT INTO "oop003" (ult_value, led) VALUES ($1, $2) RETURNING *', [ult_value, led]);
+        const res = await client.query('INSERT INTO "opp003" (ult_value, led) VALUES ($1, $2) RETURNING *', [ult_value, led]);
         return new Response(JSON.stringify(res.rows[0]), {
             status: 201,
             headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
@@ -28,7 +28,7 @@ export async function POST(req: Request) {
 
 export async function GET(){
     try {
-        const res = await client.query('SELECT * FROM "oop003" ORDER BY id DESC LIMIT 1');
+        const res = await client.query('SELECT * FROM "opp003" ORDER BY id DESC LIMIT 1');
         return new Response(JSON.stringify(res.rows), {
             status: 200,
             headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
